49dee0263ee17670576311a226bdfc319e22067c
JP-Reddy <guthijp.reddy@gmail.com>
2020-08-13 16:25:58 +0530

    Updates

---
+++ precache-manifest.1d71a46a8d0c938c9d5095cb57c398eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038fb13d3a28efe9a5200f52d341d777",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "8a95474a98f3c149bbeb",
+    "url": "/todo-react-app/static/css/main.09e834c5.chunk.css"
+  },
+  {
+    "revision": "2689bdea31c35106426a",
+    "url": "/todo-react-app/static/js/2.3406ef5f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-react-app/static/js/2.3406ef5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a95474a98f3c149bbeb",
+    "url": "/todo-react-app/static/js/main.29b925b6.chunk.js"
+  },
+  {
+    "revision": "7573a91d5d1a48b18b28",
+    "url": "/todo-react-app/static/js/runtime-main.43a17fe6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
